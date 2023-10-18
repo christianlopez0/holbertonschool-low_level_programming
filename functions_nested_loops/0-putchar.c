044c361cfc140cf56ae507c301b7886013a1f79a
Brook <christianlopezrivera0@gmail.com>
2023-10-18 09:54:13 -0400

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,16 @@
-#include <main.h>
-int main () {
-	_putchar("_putchar\n");
-	return 0;
-}	
+#include "main.h"
+
+int main (void) {
+	char *c;
+
+		int i;
+	c = "_putchar";
+
+for (i = 0; i< 9; i++)
+{
+	_putchar (c[i]);
+}
+_putchar('\n');
+return(0);
+
+}
