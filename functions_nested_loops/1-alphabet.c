b2a3d96fd0071af3b830469b9ac0a73bc94e1d01
Brook <christianlopezrivera0@gmail.com>
2023-10-19 10:09:15 -0400

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints the string "alphabet in lowercase"
+ * Return: 0 on success.
+ */
+	int main (void) 
+{
+
+	char alphabet ='a'
+
+		;while (alphabet<= 'z')
+{
+
+	_putchar(alphabet);
+
+	alphabet++;
+	}
+
+_putchar('\n');
+return (0);
+}
