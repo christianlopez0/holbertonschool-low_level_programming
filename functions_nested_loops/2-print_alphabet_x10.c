f565c6f4a905c0c27f1293e85fc3296b4ec3846e
Brook <christianlopezrivera0@gmail.com>
2023-10-19 10:43:40 -0400

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet  x10 with _putchar
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char c;
+int i:
+
+	for (i =0; i < 10; i++)
+{
+for (c = 'a' ; c <= 'z'; c++)
+{
+
+_putchar (c);
+}
+
+_putchar ('\n');
+
+
+}
+}
