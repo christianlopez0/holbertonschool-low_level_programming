863d77c7b8ee786f85fbb56b7d4d7a5b2e3e5cd7
Brook <christianlopezrivera0@gmail.com>
2023-10-20 16:20:10 -0400

    functions 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - checks for integer sign
+ * @n: integer to check
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int main(void)
-{
-  int r;
 
-  r = print_sign(98);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0xff);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(-1);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
 }
