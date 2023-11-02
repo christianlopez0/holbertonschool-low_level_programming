19b6f845f5a4c60e3e50c0ddce4351f728714520
Brook <christianlopezrivera0@gmail.com>
2023-11-02 04:41:17 -0400

    recursions 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * _puts_recursion - print a string
  * @s:string
@@ -8,12 +9,13 @@ void _puts_recursion(char *s)
 {
 if (*s)
 {
-	_putchar(*s);
+	putchar(*s);
+	
 	_puts_recursion(s + 1);
 }
 else
 {
-	_putchar('\n');
+	putchar('\n');
 }
 
 }
